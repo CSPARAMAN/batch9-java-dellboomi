45bbb74124d916330d0c1978cc3b0e4aad0ce1ca
Prasad <prasad.yoaspire@gmail.com>
2022-02-14 12:12:42 +0530

    jpa CURD operations added.

---
+++ jpademo/src/main/java/com/abc/jpademo/entity/Product.java
@@ -0,0 +1,52 @@
+package com.abc.jpademo.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product_tbl")
+public class Product {
+
+	@Id
+	@Column(name="product_id")
+	private int productId;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	private double price;
+	
+	@Column(name="created_at")
+	private LocalDate createdAt;	
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+}
---
+++ jpademo/src/main/java/com/abc/jpademo/main/SaveProductMain.java
@@ -0,0 +1,39 @@
+package com.abc.jpademo.main;
+
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.abc.jpademo.entity.Product;
+
+public class SaveProductMain {
+
+	public static void main(String[] args) {
+		
+		Product product = new Product();
+		product.setProductId(20);
+		product.setProductName("Samsung M30");
+		product.setPrice(36000);
+		product.setCreatedAt(LocalDate.of(2020, 01,10));
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction txn = em.getTransaction();
+		txn.begin();
+		
+		em.persist(product);
+		
+		txn.commit();
+		
+		System.out.println("Product Saved.");
+		
+		em.close();
+		emf.close();		
+
+	}
+
+}
